d91e3658d7a9b9941f719c152168c1d98336e286
KristinaZoteva <kristina1994misha@gmail.com>
2022-11-02 16:14:45 +0400

    Feat: add new pwa.

--- pwa/flutter_service_worker.js
+++ pwa/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "131c7324aad6ece742b9512641ecdf4a",
-"index.html": "5d456b11d9de1f150d172d356120d027",
-"/": "5d456b11d9de1f150d172d356120d027",
+"index.html": "6ea417da6c5a94bb3d3b192fcc8643c3",
+"/": "6ea417da6c5a94bb3d3b192fcc8643c3",
 "main.dart.js": "e5df4f937f3c5e0f4f365d5902ce6c47",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
